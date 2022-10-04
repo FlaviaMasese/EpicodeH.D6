b336541784cf0e6f7f70052df29cbbdc61aea703
MrsOye <flaviabosibori@gmail.com>
2022-10-04 15:26:06 +0200

    creating algorithms

---
+++ index.js
@@ -0,0 +1,22 @@
+let word = 'Hello,'
+let space = ' '
+let secondWord = 'my'
+let combinedWords= word + space + secondWord
+let thirdWord = 'name'
+let twoCombinedWords=combinedWords + space + thirdWord
+let forthWord= 'is'
+let allCombinedWords= twoCombinedWords + space + forthWord
+let firstName = 'Flavia' 
+let wordsFirstNames= allCombinedWords + space + firstName
+let secondName = 'Bosibori'
+let secondFirstName= wordsFirstNames + space + secondName
+let thirdName = 'Masese'
+let allCombinedNamesWords= secondFirstName + space + thirdName
+
+
+console.log(allCombinedNamesWords)
+
+let age = 30
+let currentYear = 2022
+let birthYear = currentYear - age
+console.log(birthYear)
