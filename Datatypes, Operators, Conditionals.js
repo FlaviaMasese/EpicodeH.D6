49651bcfd8a92d5d72b831839f8e8fa8a857c6a4
MrsOye <flaviabosibori@gmail.com>
2022-10-05 14:17:18 +0200

    datatypes

---
+++ Datatypes, Operators, Conditionals.js
@@ -0,0 +1,75 @@
+/*
+ASSIGNMENT RULES
+- All the answers must be written in JavaScript
+- You can ask for help, reach the Teaching Assistants if needed
+- You can Google / use StackOverflow BUT only when you think you need something we didn't cover during lessons yet
+- You can test your code in a separate file or de-commenting the single exercises in this one.
+- You can use the bash terminal, the VSCode terminal or the one embedded in your Operating System if you're using macOS or Linux.
+- The solution must be available for the tutors by the end of the day (5PM CET)
+*/
+
+/* EXERCISE 1
+ Enumerate and describe the main datatypes in JavaScript. Try to explain the concepts as you were speaking to a 12 years old.
+*/
+//string- is a series of characters or are words e.g. 'Flavia' they are usually written in quotes */
+//Nummbers/Integers - which are wriyyen with or without decimals.
+//Booleans- represented by two values True or False
+//Objects- used to store collections of data and more complex entities
+//Null- for unknown values
+//undefined- used for unassigned values/a variable without a value
+//symbol- used for unique identifiers 
+
+/* EXERCISE 2
+ Try to describe what a variable is, in your own words.
+*/
+
+/* // A variable is a container for storing data values. Declared by var,let or const keyword */
+
+/* EXERCISE 3
+ Write the code to execute an addition (a sum) of the numbers 12 and 20.
+*/let x= 12
+let y= 20
+let total= x+y;
+console.log(total)
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXERCISE 4
+ Create a variable named x and assign to it the number 12.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXERCISE 5
+ Create a variable called name and assign to it the string "John Doe".
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXERCISE 6
+ Execute a subtraction between the number 12 and the variable x you declared before (which is storing the value 12).
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXERCISE 7
+ Create two variables: name1 and name2. Assign to name1 the string "john", and to name2 the string "John" (this one has a capital J!)
+ Verify that name1 is different from name2 (HINT: check their equality to be false).
+ Finally, verify that the equality between name1 and name2 becomes true if both are lowercase (without changing the value of name2).
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXERCISE 8
+ Create a variable named x, and assign to it a numeric value less than 10.
+ Write the code to print the literal value of x (ex.: if x is 1, it should print "one", if x is 5 it should print "five" etc.).
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXERCISE 9
+ [Extra] Assign a value to a variable using a ternary operator (this topic has not been covered during lesson yet,
+ try to search it by yourself, tomorrow morning we'll discuss it together!)
+*/
+
+/* WRITE YOUR ANSWER HERE */
